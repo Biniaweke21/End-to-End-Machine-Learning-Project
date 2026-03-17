c5c6d46656ec3397cc0d092b617d5aaa450b84b4
Biniaweke21 <biniaweke2194@gmail.com>
2026-03-17 10:08:32 +0300

    EDA and MODEL TRAINING

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 from typing import List
 
-def get_requirements(file_path:str)->list[str]:
+def get_requirements(file_path:str)->List[str]:
     '''This function will return the list of requirements'''
     HYPHEN_E_DOT = "-e ."
 
